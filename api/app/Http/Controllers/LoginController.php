53a87f814a47f3c70ef0d9d2637c78d560188633
stalinrangel <e.stalinrangel@gmail.com>
2020-04-09 03:14:36 -0430

    ingre8

--- api/app/Http/Controllers/LoginController.php
+++ api/app/Http/Controllers/LoginController.php
@@ -85,7 +85,7 @@ class LoginController extends Controller
             }
 
             $fechas = json_decode(json_encode($fechas), true);
-
+            return count($fechas);
             for ($i=0; $i < count($fechas); $i++) { 
                for ($j=0; $j < count($usuarios); $j++) { 
                    if ($fechas[$i]->fecha==$usuarios[$j]->fecha) {
