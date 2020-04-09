4addf7d3b5492ba11222c857875d61311322dbfc
stalinrangel <e.stalinrangel@gmail.com>
2020-04-09 02:43:20 -0430

    ingre1

--- api/app/Http/Controllers/LoginController.php
+++ api/app/Http/Controllers/LoginController.php
@@ -60,7 +60,23 @@ class LoginController extends Controller
                 ':telefono' => $request->input('telefono'),
                 ':passw' => $request->input('password'),
             ]);
-            return response()->json(['usuarios'=>$usuarios], 200);
+            $fecha=[];
+            $band=false;
+            for ($i=0; $i < count($usuarios); $i++) {
+                if (count($fecha)==0) {
+                        array_push($fecha,$usuarios[$i]->fecha);
+                    }
+                for ($j=0; $j < count($fecha); $j++) { 
+                    if ($fecha[$j]==$usuarios[$i]->fecha) {
+                        $band=true;
+                    }
+                }
+                if ($band==false) {
+                    array_push($fecha,$usuarios[$i]->fecha);
+                }
+
+            }
+            return response()->json(['usuarios'=>$usuarios,'fecha'=>$fecha], 200);
 
             if($usuarios[0]->idUsuario == 'Correo inexistente'){
                 return response()->json(['error'=>'No existe el usuario.'], 404);          
@@ -96,39 +112,23 @@ class LoginController extends Controller
             
 
             $usuarios=DB::select(DB::raw("exec sp_01_ingresar :IdUsuario, :Idioma, :telefono, :passw"),[
-	            ':IdUsuario' => $request->input('IdUsuario'),
-	            ':Idioma' => 'ESP',
-	            ':telefono' => $request->input('telefono'),
-	            ':passw' => $request->input('password'),
-	        ]);
+                ':IdUsuario' => $request->input('IdUsuario'),
+                ':Idioma' => 'ESP',
+                ':telefono' => $request->input('telefono'),
+                ':passw' => $request->input('password'),
+            ]);
 
-            $fecha=[];
-            $band=false;
-            for ($i=0; $i < count($usuarios); $i++) {
-                if (count($fecha)==0) {
-                        array_push($fecha,$usuarios[$i]->fecha);
-                    }
-                for ($j=0; $j < count($fecha); $j++) { 
-                    if ($fecha[$j]==$usuarios[$i]->fecha) {
-                        $band=true;
-                    }
-                }
-                if ($band==false) {
-                    array_push($fecha,$usuarios[$i]->fecha);
-                }
+            
 
-            }
-            return response()->json(['usuarios'=>$usuarios,'fecha'=>$fecha], 200);
-
-	        if($usuarios[0]->idUsuario == 'Correo inexistente'){
-	            return response()->json(['error'=>'No existe el usuario.'], 404);          
-	        }else if($usuarios[0]->idUsuario == 'Password erroneo'){
+            if($usuarios[0]->idUsuario == 'Correo inexistente'){
+                return response()->json(['error'=>'No existe el usuario.'], 404);          
+            }else if($usuarios[0]->idUsuario == 'Password erroneo'){
                 return response()->json(['error'=>'Contraseña incorrecta.'], 404);          
             }else{
                 $user = User::where('eMail', $request->input('email'))->first();
                 $token = JWTAuth::fromUser($user);
-	            return response()->json(['usuarios'=>$usuarios[0]->idUsuario,'token'=>$token], 200);
-	        } 
+                return response()->json(['usuarios'=>$usuarios[0]->idUsuario,'token'=>$token], 200);
+            } 
         }else{
             return response()->json(['error'=>'Faltan datos para el proceso del login.'], 404);   
         }
